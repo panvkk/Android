9bbe86930c75b11c72b52b2c0dfdb0f0768a7990
panvkk <pnvk1248@gmail.com>
2025-03-26 20:26:11 +0300

    smth did but photo searcher isn't working

--- Practice/Amphibians/app/build.gradle.kts
+++ Practice/Amphibians/app/build.gradle.kts
@@ -56,4 +56,12 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    //Retrofit
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("io.coil-kt:coil-compose:2.4.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
+
 }
